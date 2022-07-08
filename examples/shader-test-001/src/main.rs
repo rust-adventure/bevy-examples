004e23700dd2165bf9f98e617712589817ff954b
Christopher Biscardi <chris@christopherbiscardi.com>
2022-07-07 18:44:23 -0700

    init

---
+++ examples/shader-test-001/src/main.rs
@@ -0,0 +1,97 @@
+//! A shader and a material that uses it.
+
+use bevy::{
+    prelude::*,
+    reflect::TypeUuid,
+    render::render_resource::{AsBindGroup, ShaderRef},
+};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(
+            MaterialPlugin::<CustomMaterial>::default(),
+        )
+        .add_startup_system(setup)
+        .add_system(change_color)
+        .run();
+}
+
+#[derive(Component)]
+struct Cube;
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // cube
+    commands
+        .spawn()
+        .insert_bundle(MaterialMeshBundle {
+            mesh: meshes
+                .add(Mesh::from(shape::Cube { size: 1.0 })),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            material: materials.add(CustomMaterial {
+                color: Color::BLUE,
+                color_texture: Some(
+                    asset_server.load("branding/icon.png"),
+                ),
+                alpha_mode: AlphaMode::Blend,
+            }),
+            ..default()
+        })
+        .insert(Cube);
+
+    // camera
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn change_color(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    cube_query: Query<Entity, With<Cube>>,
+    time: Res<Time>,
+) {
+    let cube = commands.entity(cube_query.single());
+    for material in materials.iter_mut() {
+        material.1.color =
+            if time.seconds_since_startup().floor() % 2.
+                == 0.
+            {
+                Color::RED
+            } else {
+                Color::BLUE
+            };
+    }
+}
+
+/// The Material trait is very configurable, but comes with sensible defaults for all methods.
+/// You only need to implement functions for features that need non-default behavior. See the Material api docs for details!
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material.wgsl".into()
+    }
+
+    fn alpha_mode(&self) -> AlphaMode {
+        self.alpha_mode
+    }
+}
+
+// This is the struct that will be passed to your shader
+#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct CustomMaterial {
+    #[uniform(0)]
+    color: Color,
+    #[texture(1)]
+    #[sampler(2)]
+    color_texture: Option<Handle<Image>>,
+    alpha_mode: AlphaMode,
+}
